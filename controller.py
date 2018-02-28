a7f153cea8b7c81e3c5129d2e7aaa4dc71da4ddd
Abdurrahman <abmmoham@ucsc.edu>
2018-02-28 13:45:18 -0800

    clean up everything

--- controller.py
+++ controller.py
@@ -2,10 +2,10 @@ from bottle import *
 
 @route('/index')
 def serveFile():
-  return static_file('index.html', root='./')
+  return static_file('index.html', root='./static')
 
 @route('/<filename>')
 def serveFile(filename):
-    return static_file(filename, root='./')
+    return static_file(filename, root='./static')
 
 run(host='localhost', port=8080, reloader=True)
