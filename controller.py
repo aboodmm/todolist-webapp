722e9f6bee44b6a01efd8a98e4edbd3f126b5a22
Abdul Mohamed <aboodmm@gmail.com>
2018-03-01 13:00:07 -0800

    fixed table and add done marker

--- controller.py
+++ controller.py
@@ -1,6 +1,14 @@
 import sqlite3
 from bottle import *
 
+dueasc = True
+postedasc = True
+updatedasc = True
+
+@route("/")
+def redirectindex():
+  redirect("/index")
+
 @route("/index")
 def serveFile():
   return static_file("index.html", root="./static")
@@ -20,8 +28,32 @@ def todo_list():
 def todo_list(filters):
   conn = sqlite3.connect("todo.db")
   c = conn.cursor()
+  global dueasc, postedasc, updatedasc
   if filters == "due":
-    c.execute("SELECT * FROM todo ORDER BY due")
+    if dueasc:
+      c.execute("SELECT * FROM todo ORDER BY due ASC")
+      dueasc = False
+    else:
+      c.execute("SELECT * FROM todo ORDER BY due DESC")
+      dueasc = True
+  if filters == "posted":
+    if postedasc:
+      c.execute("SELECT * FROM todo ORDER BY posted ASC")
+      postedasc = False
+    else:
+      c.execute("SELECT * FROM todo ORDER BY posted DESC")
+      postedasc = True
+  if filters == "updated":
+    if updatedasc:
+      c.execute("SELECT * FROM todo ORDER BY updated ASC")
+      updatedasc = False
+    else:
+      c.execute("SELECT * FROM todo ORDER BY updated DESC")
+      updatedasc = True
+  if filters == "done":
+    c.execute("SELECT * FROM todo WHERE status = 1")
+  if filters == "notdone":
+    c.execute("SELECT * FROM todo WHERE status = 0")
   result = c.fetchall()
   c.close()
   
@@ -33,6 +65,15 @@ def delete_item(item_id):
   conn = sqlite3.connect("todo.db")
   c = conn.cursor()
   c.execute("DELETE from todo WHERE id = ?", (item_id))
+  conn.commit()
+  c.close()
+  redirect("/todolist")
+
+@route("/update/<item_id>")
+def update_item(item_id):
+  conn = sqlite3.connect("todo.db")
+  c = conn.cursor()
+  c.execute("UPDATE todo set status = 1 WHERE id = ?", (item_id))
   conn.commit()
   c.close()
   redirect("/todolist")
--- make_table.tpl
+++ make_table.tpl
@@ -1,5 +1,8 @@
+<h1>cmps183: Homework 3</h1>
+<h2><a href="/">Home</a> <a href="/todolist">To Do List</a>
+<a href="/todoform">To Do Form</a></h2>
+
 <table>
-%for row in rows:
   <tr>
     <td>Id</td>
     <td>Task</td>
@@ -9,9 +12,15 @@
     <td>Date updated</td>
     <td>Done?</td>
   </tr>
+%for row in rows:
   %for col in row:
     <td>{{col}} </td>
   %end
+  <td>
+    <button onclick="parent.location.href=
+    'http://localhost:8080/update/{{row[0]}}'"
+    type="button">Mark done</button>
+  </td>
   <td>
     <button onclick="parent.location.href=
     'http://localhost:8080/edit/{{row[0]}}'"
@@ -27,5 +36,26 @@
 </table>
 
 <button onclick="parent.location.href=
+'http://localhost:8080/todolist'"
+type="button">Show all</button>
+
+<button onclick="parent.location.href=
+'http://localhost:8080/todolist/done'"
+type="button">Show completed</button>
+
+<button onclick="parent.location.href=
+'http://localhost:8080/todolist/notdone'"
+type="button">Show todo</button>
+
+<button onclick="parent.location.href=
 'http://localhost:8080/todolist/due'"
 type="button">Sort by due date</button>
+
+<button onclick="parent.location.href=
+'http://localhost:8080/todolist/posted'"
+type="button">Sort by posted date</button>
+
+<button onclick="parent.location.href=
+'http://localhost:8080/todolist/updated'"
+type="button">Sort by updated date</button>
+
