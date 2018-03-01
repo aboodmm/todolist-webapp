916a7ade78280b7f34250f59e0fd33f746619a98
Abdul Mohamed <aboodmm@gmail.com>
2018-03-01 11:32:41 -0800

    add db backup and get delete done

--- controller.py
+++ controller.py
@@ -1,20 +1,30 @@
 import sqlite3
 from bottle import *
 
-@route('/index')
+@route("/index")
 def serveFile():
-  return static_file('index.html', root='./static')
+  return static_file("index.html", root="./static")
 
-@route('/todolist')
+@route("/todolist")
 def todo_list():
-  conn = sqlite3.connect('todo.db')
+  conn = sqlite3.connect("todo.db")
   c = conn.cursor()
   c.execute("SELECT * from todo")
   result = c.fetchall()
   c.close()
 
-  output = template('make_table', rows=result)
+  output = template("make_table", rows=result)
   return output
 
-run(host='localhost', port=8080, reloader=True)
+@route("/delete/<item_id>")
+def delete_item(item_id):
+  conn = sqlite3.connect("todo.db")
+  c = conn.cursor()
+  c.execute("DELETE from todo WHERE id = ?", (item_id,))
+  conn.commit()
+  c.close()
+  redirect("/todolist")
+  
+
+run(host="localhost", port=8080, reloader=True)
 
--- make_table.tpl
+++ make_table.tpl
@@ -1,9 +1,27 @@
 <table>
 %for row in rows:
   <tr>
+    <td>Id</td>
+    <td>Task</td>
+    <td>Description</td>
+    <td>Date posted</td>
+    <td>Date due</td>
+    <td>Date updated</td>
+    <td>Done?</td>
+  </tr>
   %for col in row:
     <td>{{col}} </td>
   %end
+  <td>
+    <button onclick="parent.location.href=
+    'http://localhost:8080/edit/{{row[0]}}'"
+    type="button">Edit</button>
+  </td>
+  <td>
+    <button onclick="parent.location.href=
+    'http://localhost:8080/delete/{{row[0]}}'"
+    type="button">Delete</button>
+  </td>
   </tr>
 %end
 </table>
